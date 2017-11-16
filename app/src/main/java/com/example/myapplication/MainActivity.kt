5159faa5fe18d55e7aba847eb1a5a89e10928f7e
Evin1- <edwin.hdz.12.0@gmail.com>
2017-11-15 20:02:08 -0500

    Add simple LiveData logic

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+  private val liveName = MutableLiveData<String>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val observer: Observer<String> = Observer { a_main_txt.text = it }
+    liveName.observe(this, observer)
+
+    a_main_modify.setOnClickListener { liveName.value = Date().toString() }
   }
 }
